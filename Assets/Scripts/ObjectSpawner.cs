8c3fd86a80d5899c833d201f687e3a2c75119b50
AfsinKaratas <afsinkaratas@gmail.com>
2020-02-01 00:38:24 +0300

    Object spawners added.

---
+++ Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+
+	public GameObject itemPrefab;
+	public Vector2 forwardForceInterval;
+	public Vector2 upwardForceInterval;
+	public Vector2 sidewaysForceInterval;
+	public int hitPoints;
+	public float cooldown;
+
+	private Rigidbody itemRB;
+	private float interactTime;
+	private int currentHitPoints;
+
+	private void Start()
+	{
+		interactTime = -cooldown;
+		currentHitPoints = hitPoints;
+	}
+
+	private void SpawnItem()
+	{
+		interactTime = Time.time;
+		GameObject itemGO = Instantiate(itemPrefab, transform);
+		itemRB = itemGO.GetComponent<Rigidbody>();
+		float forwardForce = Random.Range(forwardForceInterval.x, forwardForceInterval.y);
+		float upwardsForce = Random.Range(upwardForceInterval.x, upwardForceInterval.y);
+		float sidewaysForce = Random.Range(sidewaysForceInterval.x, sidewaysForceInterval.y);
+		itemRB.AddForce(transform.forward * forwardForce + transform.up * upwardsForce +
+			transform.right * sidewaysForce);
+	}
+
+	public void Hit()
+	{
+		if (Time.time - interactTime < cooldown) return;
+
+		currentHitPoints--;
+		if (currentHitPoints <= 0)
+		{
+			SpawnItem();
+			currentHitPoints = hitPoints;
+		}
+	}
+
+}
