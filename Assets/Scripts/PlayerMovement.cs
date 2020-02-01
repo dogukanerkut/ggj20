a72e59aeb2ee48b2133959dcbbaf2f69710a22bd
Dogukan <dogukanerkut@gmail.com>
2020-02-01 13:47:40 +0300

    Character rotation setup

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,42 +7,62 @@ public class PlayerMovement : MonoBehaviour
 
     public bool joystick;
     public float speed;
-    public Transform character;
+    public Transform _characterParent;
+    public Transform _character;
     public float rotationLerpSpeed;
 
     private Rigidbody playerRB;
-    private Vector3 lookAtTarget;
-
+    private Quaternion lookRotation;
+    private Vector3 _inputDir;
+    [SerializeField] private Vector3 axis;
     private void Start()
     {
         playerRB = GetComponent<Rigidbody>();
     }
+    // private void Update()
+    // {
+    //     RaycastHit hitInfo = new RaycastHit();
+    //     Physics.Raycast(transform.position, Vector3.down, out hitInfo, 10);
+    //     if (hitInfo.collider != null)
+    //     {
+    //     //    Quaternion lookAt = Quaternion.LookRotation((hitInfo.normal).normalized, axis);
+           
+    //     //    _character.localRotation = Quaternion.Euler(lookAt.eulerAngles.x - 90, lookAt.eulerAngles.y, lookAt.eulerAngles.z);
+    //     Debug.Log(hitInfo.collider.gameObject.name);
+    //     Debug.Log(hitInfo.normal);
+    //         _character.up = Vector3.Lerp(_character.up, hitInfo.normal, Time.deltaTime * 8f);
 
+    //     }
+    // }
     private void FixedUpdate()
     {
+        float horizontal = joystick ? Input.GetAxis("Horizontal1") : Input.GetAxisRaw("Horizontal");
+        float vertical = joystick ? Input.GetAxis("Vertical1") : Input.GetAxisRaw("Vertical");
+        _inputDir = new Vector3(horizontal, 0, vertical).normalized;
+        if (!IsReceivingInput())
+        {
+            playerRB.isKinematic = true;
+            return;
+        }
+        playerRB.isKinematic = false;
         MovementInput();
         LookAt();
     }
 
     public void MovementInput()
     {
-        float horizontal = joystick ? Input.GetAxis("Horizontal1") : Input.GetAxis("Horizontal");
-        float vertical = joystick ? Input.GetAxis("Vertical1") : Input.GetAxis("Vertical");
-        Vector3 moveVector = Vector3.zero;
-        if (horizontal < 0f) moveVector -= transform.right;
-        if (horizontal > 0f) moveVector += transform.right;
-        if (vertical < 0f) moveVector -= transform.forward;
-        if (vertical > 0f) moveVector += transform.forward;
-        lookAtTarget = transform.position + moveVector;
-        moveVector = moveVector * speed * Time.deltaTime;
+        Vector3 moveVector = _inputDir * speed * Time.fixedDeltaTime;
         playerRB.velocity = new Vector3(moveVector.x, playerRB.velocity.y, moveVector.z);
+    }
+    private bool IsReceivingInput()
+    {
+        return _inputDir.magnitude > 0;
     }
 
     private void LookAt()
     {
-        Vector3 target = Vector3.Lerp(character.position + character.forward, lookAtTarget,
-            Time.deltaTime * rotationLerpSpeed);
-        character.LookAt(target);
+        lookRotation = Quaternion.LookRotation(_inputDir, Vector3.up);
+        _characterParent.transform.rotation = Quaternion.Slerp(_characterParent.transform.rotation, lookRotation, rotationLerpSpeed * Time.fixedDeltaTime);
     }
 
 }
