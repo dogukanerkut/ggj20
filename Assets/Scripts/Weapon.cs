19d18687c27c3df95a394d0af9386465a742f941
AfsinKaratas <afsinkaratas@gmail.com>
2020-02-01 12:42:06 +0300

    IInteractable added. Hit anim triggered in player.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IInteractable interactable = other.GetComponent<IInteractable>();
+        if (interactable != null)
+        {
+            interactable.Interact();
+        }
+    }
+
+}
