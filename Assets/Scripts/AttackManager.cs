1bc7f389a72538c56f358d62a0b9d48e373bd4f2
Dogukan <dogukanerkut@gmail.com>
2020-02-01 20:23:43 +0300

    Add random attack system

--- Assets/Scripts/AttackManager.cs
+++ Assets/Scripts/AttackManager.cs
@@ -6,4 +6,15 @@ public class AttackManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private List<Attacker> _attackers;
+    [SerializeField] private AttackData _data;
+    private void Awake()
+    {
+        StartCoroutine(DelayedAttack(_data.DelayBeforeFirstAttack));
+    }
+    private IEnumerator DelayedAttack(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        _attackers[Random.Range(0, _attackers.Count)].Attack();
+        StartCoroutine(DelayedAttack(_data.AttackFrequency));
+    }
 }
