6d8c0979e4025908d226e1b57317694e93f3f94b
Utkan Guder <utkan96@gmail.com>
2020-02-02 08:52:43 +0300

    added gameover and reload buttn

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+
+    public Animator canvasAnim;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void GameOver()
+    {
+        canvasAnim.SetBool("gameover", true);
+    }
+
+    public void ReloadGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/TimeControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControl : MonoBehaviour
+{
+    public static TimeControl instance;
+
+    private float targetTimeScale = 1;
+
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        targetTimeScale = 1;
+    }
+
+    private void Update()
+    {
+        Time.timeScale = Mathf.Lerp(Time.timeScale, targetTimeScale, Time.unscaledDeltaTime * 10);
+    }
+
+    public void SetTimeScale(float scale)
+    {
+        targetTimeScale = scale;
+    }
+}
